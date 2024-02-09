9fe820c3bdbb592a9e0f3525978610e8b00fbb68
EtiBarr <etienne.barriere.1@ens.etsmtl.ca>
2024-02-08 19:54:41 -0500

    connexion done

--- scripts/connexion.php
+++ scripts/connexion.php
@@ -6,10 +6,10 @@ function connexionPDO($prenom, $nom, $couriel, $psw) {
 
             // Établir la connexion avec PDO
             $conn = new PDO("mysql:host=$prenom;dbname=$nom", $courielle, $psw);
-      
+
             // Activer le mode d'erreur
             $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-      
+
       } catch(PDOException $e) {
 
       // Retourner une valeur vide en cas d'échec de la connexion
@@ -20,6 +20,7 @@ function connexionPDO($prenom, $nom, $couriel, $psw) {
 }
 
 */
+/*
 // Paramètres de connexion à la base de données
 $hostname = "localhost";
 $identifiant = "admin";
@@ -50,9 +51,50 @@ try {
         die("Erreur lors de l'exécution de la requête: " . $conn->errorInfo()[2]);
     }
 
-} catch(PDOException $e) { 
+} catch(PDOException $e) {
     // Arrêter le script si la connexion échoue
     die("Connexion échouée: " . $e->getMessage());
 }
+
+*/
+
+
+// Paramètres de connexion à la base de données
+//this works well
+$hostname = "localhost";
+$username = "admin";
+$password = "admin";
+$database = "tch056_projet_integrateur";
+try {
+    // Établir la connexion avec PDO
+    $conn = new PDO("mysql:host=$hostname;dbname=$database",
+                    $username,
+                    $password);
+
+    // Activer le mode d'erreur
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    // Afficher un message si la connexion est réussie
+    echo "Connexion réussie avec PDO!";
+
+
+// Perform a test query
+    $test = "SELECT * FROM utilisateurs";
+    $resultTest = $conn->query($test);
+
+    //test to see if queury worked
+    if ($resultTest !== false) {
+        // Fetch and print the results
+        $rows = $resultTest->fetchAll(PDO::FETCH_ASSOC);
+        print_r($rows);
+    } else {
+        // Print an error message if the query failed
+        die("Erreur lors de l'exécution de la requête: " . $conn->errorInfo()[2]);
+    }
+
+}   catch(PDOException $e) {// Arrêter le script si la connexion échoue
+    die("Connexion échouée: " . $e->getMessage());
+}
+
 ?>
 
